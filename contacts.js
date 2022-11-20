c58e2b8be918504016031d0aeb439e8413ab4d23
Yan <yhor450@gmail.com>
2022-11-20 23:48:22 +0200

    test

--- contacts.js
+++ contacts.js
@@ -1,60 +1,78 @@
-const fs = require('fs');
-const path = require('path');
-const contactsPath = __dirname + '/db/contacts.json';
-
-console.log(removeContact(8))
+const fs = require("fs");
+const path = require("path");
+const contactsPath = __dirname + "/db/contacts.json";
 
 // TODO: задокументувати кожну функцію
 async function listContacts() {
-    const contacts = await getContacts();
-    console.log(contacts);
+  const contacts = await getContacts();
+  console.log(contacts);
 }
 
 async function getContacts() {
-    return new Promise((resolve, reject) => {
-        fs.readFile(contactsPath, 'utf8', (error, fileContent) => {
-            if (error) {
-                throw error;
-            }
+  return new Promise((resolve, reject) => {
+    fs.readFile(contactsPath, "utf8", (error, fileContent) => {
+      if (error) {
+        throw error;
+      }
 
-            const contactsJson = fileContent.toString('utf8');
-            const parsedData = JSON.parse(contactsJson);
+      const contactsJson = fileContent.toString("utf8");
+      const parsedData = JSON.parse(contactsJson);
 
-            resolve(parsedData);
-        });
+      resolve(parsedData);
     });
+  });
 }
 
 async function getContactById(contactId) {
-    const contacts = await getContacts();
-    const filteredContact = contacts.filter(contact => contact.id == contactId);
-    return filteredContact[0];
+  const contacts = await getContacts();
+  const filteredContact = contacts.filter((contact) => contact.id == contactId);
+  return filteredContact[0];
 }
 
 async function removeContact(contactId) {
-    const contacts = await getContacts();
-    for (let i; i < contacts.length; i++) {
-        console.log(contacts[i]);
-        if (contacts[i].id == contactId) {
-            console.log(contacts[i]);
-            delete contacts[contact];
-            console.log(contacts[contact]);
-            console.log(contacts.length);
-        }
+  const contacts = await getContacts();
+  const res = [];
+  for (let contact of contacts) {
+    if (+contact.id === contactId) {
+      continue;
     }
+    res.push(contact);
+  }
+  return await writeFile(res);
 }
 
-function addContact(name, email, phone) {
-    // ...твій код
+async function writeFile(arr) {
+  return new Promise((resolve, reject) => {
+    const json = JSON.stringify(arr);
+    fs.writeFile(contactsPath, json, (err) => {
+      resolve();
+    });
+  });
 }
 
+async function addContact(name, email, phone) {
+  const contacts = await getContacts();
+  const ids = contacts.map((object) => {
+    return object.id;
+  });
+  const max = Math.max(...ids);
+  const id = max + 1;
+  const newContact = {
+    id: id.toString(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+
+  writeFile(contacts);
+}
 
 // function findOne = contacts;
 
 module.exports = {
-    listContacts,
-    getContacts,
-    getContactById,
-    removeContact,
-    addContact
-}
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
